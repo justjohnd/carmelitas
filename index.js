6307c9bd9beb95c40490365a77af0b90cced6fd3
John Denlinger <justjohnd@gmail.com>
2020-07-21 12:51:31 +0900

    add scroll spy to main page; add favicon; add menu sections to dropdown

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
+// Get current year for copyright
 $('#year').text(new Date().getFullYear());
 
+//Use keyboard to pause carousel
 $(".carousel").carousel({
     keyboard: true
+});
+
+//Init Scrollspy
+$('body').scrollspy({
+    target: '#main-nav'
+});
+
+$("#main-nav a").on('click', function (event) {
+    if (this.hash !== "") {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800, function () {
+            window.location.hash = hash;
+        });
+    }
 });
 
